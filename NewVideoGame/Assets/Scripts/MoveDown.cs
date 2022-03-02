aa6f87dd325eb60dd6ac2e688d5fd4e0a5721dd6
Pivot <payton.field@live.com>
2022-03-02 15:40:21 -0700

    ProblemsBalloon

--- NewVideoGame/Assets/Scripts/MoveDown.cs
+++ NewVideoGame/Assets/Scripts/MoveDown.cs
@@ -9,12 +9,13 @@ public class MoveDown : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-      transform.Translate(Vector3.down * Time.deltaTime, 0);  
+        transform.Translate(Vector3.down * Time.deltaTime, 0);
     }
 }
+
