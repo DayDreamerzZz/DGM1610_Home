3d347899a6548d6717a2592dc8bfd00b791d3c45
Pivot <payton.field@live.com>
2022-04-30 22:46:49 -0600

    Main menu and Level

---
+++ Akiko/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneToLoad); //Scene to Load
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); // Quit Game
+        Debug.Log("Quit Game"); // Print to Console
+    }
+}
